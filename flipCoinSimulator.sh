6250cc9a316d1e474bfddd0850f325e7d92cc01a
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-03-17 09:17:55 +0530

    Use Case - 2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,23 @@
-coin=$((RANDOM%2))
-echo "Coin Flipping ...."
-if(($coin==1))
-then
-	echo "Heads Won"
-        heads=$(($heads+1))
-else
-	echo "Tails Won"
-        tails=$(($tails+1))
-fi
+a=1
+n=40
+heads=0
+tails=0
+function coinsimulation(){
+
+        coin=$((RANDOM%2))
+        if(($coin==1))
+        then
+                echo "Heads Won"
+                heads=$(($heads+1))
+        else echo "Tails Won"
+                tails=$(($tails+1))
+        fi
+
+}
+while(($a<=$n))
+do
+        coinsimulation
+	a=$(($a+1))
+done
+echo "Heads won $heads times"
+echo "Tails won $tails times"
